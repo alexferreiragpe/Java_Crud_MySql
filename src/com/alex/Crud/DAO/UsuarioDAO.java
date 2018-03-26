93194ba7e4906a4672a2a7b5afd2bb46d72972e5
Alex Ferreira <alexferreira@hotmail.com.br>
2018-03-26 03:22:30 -0300

    Crud Java - Criando Metodos

---
+++ src/com/alex/Crud/DAO/UsuarioDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alex.Crud.DAO;
+import com.alex.Crud.DAO.UsuarioDAO;
+import com.alex.Crud.Model.Usuario;
+public class UsuarioDAO {
+    
+    public void save(Usuario usuario){
+        String sql="INSERT INTO user()" +"values";
+    }
+}
