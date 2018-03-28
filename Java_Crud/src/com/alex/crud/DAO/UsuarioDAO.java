e35b94fd147782d8cf95f9b56aad7a1ab48c05b3
Alex Ferreira <alexferreira@hotmail.com.br>
2018-03-28 18:58:20 -0300

    Crud Completo

--- Java_Crud/src/com/alex/crud/DAO/UsuarioDAO.java
+++ Java_Crud/src/com/alex/crud/DAO/UsuarioDAO.java
@@ -9,10 +9,14 @@ import com.alex.Crud.Model.Usuario;
 import com.alex.Crud.ConexaoBanco.ConexaoMySQL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
 
     public void save(Usuario usuario) {
+
         String sql = "INSERT INTO user(UserName,Senha,Nome,Email,Celular,TelFixo,Cidade,Estado)" + "values(?,?,?,?,?,?,?,?)";
         Connection conn = null;
         PreparedStatement pstm = null;
@@ -49,6 +53,7 @@ public class UsuarioDAO {
     }
 
     public void removeById(int id) {
+
         String sql = "DELETE from user where iduser=?";
         Connection conn = null;
         PreparedStatement pstm = null;
@@ -62,7 +67,7 @@ public class UsuarioDAO {
             e.printStackTrace();
         } finally {
             try {
-                if (pstm !=null) {
+                if (pstm != null) {
                     System.out.println("Usuário Excluído com Sucesso!");
                     pstm.close();
                 }
@@ -73,6 +78,97 @@ public class UsuarioDAO {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void update(Usuario usuario) {
+
+        String sql = "UPDATE user SET UserName=?,Senha=?,Nome=?,Email=?,Celular=?,TelFixo=?,Cidade=?,Estado=?"
+                + "where iduser=?";
+
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        try {
+            conn = ConexaoMySQL.createConnectionToMySQL();
+            pstm = conn.prepareStatement(sql);
+
+            pstm.setString(1, usuario.getUserName());
+            pstm.setString(2, usuario.getSenha());
+            pstm.setString(3, usuario.getNome());
+            pstm.setString(4, usuario.getEmail());
+            pstm.setString(5, usuario.getCelular());
+            pstm.setString(6, usuario.getTelFixo());
+            pstm.setString(7, usuario.getCidade());
+            pstm.setString(8, usuario.getEstado());
+            pstm.setInt(9, usuario.getIdUser());
+            pstm.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstm != null) {
+                    System.out.println("Usuário Atualizado com Sucesso!");
+                    pstm.close();
+                }
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Usuario> getUsuarios() {
+
+        String sql = "SELECT * FROM user";
+
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        ResultSet rset = null;
+
+        try {
+            conn = ConexaoMySQL.createConnectionToMySQL();
+            pstm = conn.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setIdUser(rset.getInt("IdUser"));
+                usuario.setUserName(rset.getString("UserName"));
+                usuario.setNome(rset.getString("Nome"));
+                usuario.setEmail(rset.getString("Email"));
+                usuario.setCelular(rset.getString("Celular"));
+                usuario.setTelFixo(rset.getString("TelFixo"));
+                usuario.setCidade(rset.getString("Cidade"));
+                usuario.setEstado(rset.getString("Estado"));
+
+                usuarios.add(usuario);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rset != null) {
+                    System.out.println("Usuários Listados com Sucesso!\n");
+                    rset.close();
+                }
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return usuarios;
     }
 
 }
--- Java_Crud/src/com/alex/crud/operacao/operacao.java
+++ Java_Crud/src/com/alex/crud/operacao/operacao.java
@@ -5,8 +5,6 @@ package com.alex.crud.operacao;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import com.alex.Crud.DAO.UsuarioDAO;
 import com.alex.Crud.Model.Usuario;
 
@@ -15,25 +13,42 @@ import com.alex.Crud.Model.Usuario;
  * @author alexferreira
  */
 public class operacao {
-    
-    
-    public static void main(String args[]){
-        UsuarioDAO usuarioDao=new UsuarioDAO();
-        Usuario usuario=new Usuario();
-     
-        
-        
-        usuario.setUserName("dfd");
+
+    public static void main(String args[]) {
+        UsuarioDAO usuarioDao = new UsuarioDAO();
+        Usuario usuario = new Usuario();
+
+        //Cria um Usuario e salva no banco//
+        usuario.setUserName("alexferreira2");
         usuario.setSenha("90899");
-        usuario.setNome("Gadnodfd");
-        usuario.setEmail("cdfsdfdasdfrisdfd141@uol.com.br");
+        usuario.setNome("Alex Ferreiraa");
+        usuario.setEmail("frisdfd14a1@uol.com.br");
         usuario.setCelular("19-99760-9890");
         usuario.setTelFixo("19-3789-7689");
         usuario.setCidade("Guaxupé");
         usuario.setEstado("MG");
         usuarioDao.save(usuario);
         
-        usuarioDao.removeById(27);
+        //Remove o Usuario com idUser=//
+        //usuarioDao.removeById(27);
+        //Atualiza o Usuario com id = "" com os dados do objeto user//        
+        /*Usuario user = new Usuario();
+        user.setIdUser(27);
+        user.setUserName("teste");
+        user.setSenha("90d899");
+        user.setNome("Cristina");
+        user.setEmail("cristina@uol.com.br");
+        user.setCelular("19-99760-9890");
+        user.setTelFixo("19-3789-7689");
+        user.setCidade("Guaxupé");
+        user.setEstado("MG");                
+        usuarioDao.update(user);
+         */
+        //Lista todos os contatos do banco de dados//
+        for (Usuario u : usuarioDao.getUsuarios()) {
+            System.out.println("Usuário -->  " + u.getUserName());
+        }
+
     }
-   
+
 }
